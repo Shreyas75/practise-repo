61f70278c522028407e739c6ea86441a59c0cfe0
Shreyas75 <manotishreyas@gmail.com>
2024-08-16 20:39:44 +0530

    new add

--- hitesh-next-auth/src/app/api/users/signup/route.ts
+++ hitesh-next-auth/src/app/api/users/signup/route.ts
@@ -10,24 +10,29 @@ export async function POST(request: NextRequest) {
     try {
         const reqBody = await request.json()
         const {username, email, password} = reqBody
-        // Validation
+        // Validation here
         console.log(reqBody)
 
+        // checking if user exists
         const user = await User.findOne({email})
 
+        // if exists please return
         if(user) {
             return NextResponse.json({error: "User already exists"}, {status: 400});
         }
 
+        // If user does not exists, hash the password and continue
         const salt = await bcryptjs.genSalt(10);
         const hashedPassword = await bcryptjs.hash(password, salt)
 
+        // creating a new User
         const newUser = new User({
             username,
             email,
             password: hashedPassword,
         })
 
+        // saving the user to db
         const savedUser = await newUser.save()
         console.log(savedUser)
 
