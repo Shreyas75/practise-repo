cfb193547dcc84db6d30844edd28933c4b71a7ad
Shreyas75 <manotishreyas@gmail.com>
2024-08-18 06:59:57 +0530

    signup frontend

--- hitesh-next-auth/src/app/verifyemail/page.tsx
+++ hitesh-next-auth/src/app/verifyemail/page.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
+import axios from 'axios';
 
-const page = () => {
+export default function VerifyEmailPage () {
+  const [token, setToken] = useState("");
+  const [verified, setVerified] = useState(false);
+  const [error, setError] = useState(false);
+
+  const verifyUserEmail = async () => {
+    try {
+      await axios.post("/api/users/verifyemail", {token})
+      setVerified(true);
+    } catch (error: any) {
+      setError(true)
+      console.log(error.response.data);
+    }
+  }
   return (
-    <div>Verifyemail Page</div>
+    <div>page</div>
   )
 }
 
-export default page
