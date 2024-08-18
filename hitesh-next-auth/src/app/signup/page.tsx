4660175c680a771e71326a827ccf08226615fe66
Shreyas75 <manotishreyas@gmail.com>
2024-08-18 06:28:54 +0530

    signup frontend

--- hitesh-next-auth/src/app/signup/page.tsx
+++ hitesh-next-auth/src/app/signup/page.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from "axios";
+import {toast} from "react-hot-toast";
+export default function SignupPage() {
 
-function page() {
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+    username: ""
+  })
+
+  const [buttonDisabled, setButtonDisabled] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const onSignup = async () => {
+    try {
+      setLoading(true);
+      const response =  await axios.post("/api/users/signup", user);
+
+      console.log("Signup success", response.data);
+    } catch (error: any) {
+      console.log("Signup failed")
+
+    }
+  }
+
   return (
-    <div>signup page</div>
+    <div>Signup page</div>
   )
 }
-
-export default page
